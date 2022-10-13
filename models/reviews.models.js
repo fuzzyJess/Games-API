ae6c58c8a8052045014fd540a3a9867dae68e1ac
fuzzyJess <jrundle99@yahoo.com>
2022-10-13 11:21:20 +0100

    amend and pass test for GET api/reviews so that it now returns comment count

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -1,17 +1,25 @@
 const db = require("../db/connection");
 
 exports.selectReview = (id) => {
-    return db.query(`SELECT * FROM reviews WHERE review_id = $1`,  [id])
-    .then((data) => {
-        const review = data.rows[0];
-        if (!review) {
-            return Promise.reject({
-                status: 404,
-                msg: "Review ID not found"
-            });
-        }
-        return review;
-    })
+    return db.query(`
+    SELECT reviews.*, COUNT(comments.review_id) ::INT AS comment_count 
+    FROM reviews
+    LEFT JOIN comments 
+    ON reviews.review_id = comments.review_id
+    WHERE reviews.review_id = $1
+    GROUP BY reviews.review_id;`, [id])
+        .then((data) => {
+
+            const review = data.rows[0];
+
+            if (!review) {
+                return Promise.reject({
+                    status: 404,
+                    msg: "Review ID not found"
+                });
+            }
+            return review;
+        })
 }
 
 exports.updateReview = (id, votes) => {
@@ -28,18 +36,19 @@ exports.updateReview = (id, votes) => {
             status: 400, msg: "Wrong data type"
         });
     }
-    
+
     return db.query(`UPDATE reviews 
     SET votes = votes + $1 
     WHERE review_id = $2 RETURNING *;`, [incVotes, id])
-    .then((data) => {
-        const review = data.rows[0];
-        if (!review) {
-            return Promise.reject({
-                status: 404,
-                msg: "Review ID not found"
-            });
-        }
-        return review;
-    })
+        .then((data) => {
+            const review = data.rows[0];
+            if (!review) {
+                return Promise.reject({
+                    status: 404,
+                    msg: "Review ID not found"
+                });
+            }
+            console.log(review);
+            return review;
+        })
 }
