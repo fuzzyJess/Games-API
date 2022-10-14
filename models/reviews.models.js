1305191c0ebe32f1444b99966b74c7437903ac19
fuzzyJess <jrundle99@yahoo.com>
2022-10-14 16:29:34 +0100

    set up for query params to be passed into function

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -52,49 +52,59 @@ exports.updateReview = (id, votes) => {
         })
 }
 
-exports.selectReviews = (category) => {
-    const categoryArr = [];
-    let sqlQuery = `
-    SELECT reviews.*, COUNT(comments.review_id) ::INT AS comment_count 
-    FROM reviews
-    LEFT JOIN comments 
-    ON reviews.review_id = comments.review_id `;
+exports.selectReviews = (category, sort_by, order) => {
+   
+    // to handle category query
+   
+    if (!sort_by || order) {
+        const categoryArr = [];
+        let sqlQuery = `
+        SELECT reviews.*, COUNT(comments.review_id) ::INT AS comment_count 
+        FROM reviews
+        LEFT JOIN comments 
+        ON reviews.review_id = comments.review_id `;
 
-    if (category) {
-        if (![
-            'strategy',
-            'social deduction',
-            'hidden-roles',
-            'dexterity',
-            'push-your-luck',
-            'roll-and-write',
-            'deck-building',
-            'engine-building',
-            'euro game',
-            "children's games"
-        ].includes(category)) {
-            return Promise.reject({ status: 400, msg: 'Invalid category provided' });
-        }
-        sqlQuery += `WHERE reviews.category = $1`
-        categoryArr.push(category);
-    }
-    sqlQuery +=
-        `GROUP BY reviews.review_id
-    ORDER BY reviews.created_at DESC;`
-
-    return db.query(sqlQuery, categoryArr)
-        .then((data) => {
-
-            const reviews = data.rows;
-
-            if (!reviews) {
-                return Promise.reject({
-                    status: 404,
-                    msg: "Review ID not found"
-                });
+        if (category) {
+            if (![
+                'strategy',
+                'social deduction',
+                'hidden-roles',
+                'dexterity',
+                'push-your-luck',
+                'roll-and-write',
+                'deck-building',
+                'engine-building',
+                'euro game',
+                "children's games"
+            ].includes(category)) {
+                return Promise.reject({ status: 400, msg: 'Invalid category provided' });
             }
-            return reviews;
-        })
+            sqlQuery += `WHERE reviews.category = $1`
+            categoryArr.push(category);
+        }
+        sqlQuery +=
+            `GROUP BY reviews.review_id
+            ORDER BY reviews.created_at DESC;`
+
+        return db.query(sqlQuery, categoryArr)
+            .then((data) => {
+
+                const reviews = data.rows;
+
+                if (!reviews) {
+                    return Promise.reject({
+                        status: 404,
+                        msg: "Review ID not found"
+                    });
+                }
+                return reviews;
+            })
+        }
+
+    // to handle sort_by
+
+    
+    
 }
 
 exports.selectComments = (review_id) => {
